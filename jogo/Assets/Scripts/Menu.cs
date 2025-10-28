1cb0d4c722c97ecc3a33264f903e6c8fb2ac9c80
FabioCirne4537 <fabio.83518@alunosatc.edu.br>
2025-10-28 15:35:58 -0300

    Menu

--- jogo/Assets/Scripts/Menu.cs
+++ jogo/Assets/Scripts/Menu.cs
@@ -19,4 +19,32 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
+
+    public void ActiveConfig(GameObject go)
+    {
+        go.SetActive(true);
+    }
+
+
+    public void DisableConfig(GameObject go)
+    {
+        go.SetActive(false);
+    }
+
+    public void ActivePause(GameObject go)
+    {
+        Time.timeScale = 0;
+        go.SetActive(true);
+    }
+
+    public void DisablePause(GameObject go) {
+        Time.timeScale = 1;
+        go.SetActive(false);
+    }
+
+    public void Gotohome(GameObject go) {
+        go.SetActive(true);
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
 }
